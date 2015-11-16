d5e3592ce1a56e696e3fb9e7656eaa68ef66bbd6
Aymeric Damien <damien@immomo.com>
2015-11-16 13:11:39 +0800

    update comments

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,3 +1,11 @@
+'''
+A logistic regression learning algorithm example using TensorFlow library.
+This example is using the MNIST database of handwritten digits (http://yann.lecun.com/exdb/mnist/)
+
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
 # Import MINST data
 import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
@@ -10,33 +18,49 @@ training_epochs = 25
 batch_size = 100
 display_step = 1
 
+# tf Graph Input
+x = tf.placeholder("float", [None, 784]) # mnist data image of shape 28*28=784
+y = tf.placeholder("float", [None, 10]) # 0-9 digits recognition => 10 classes
+
 # Create model
-x = tf.placeholder("float", [None, 784])
-y = tf.placeholder("float", [None,10])
-W = tf.Variable(tf.zeros([784,10]))
+
+# Set model weights
+W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
-activation = tf.nn.softmax(tf.matmul(x,W) + b) #softmax
-cost = -tf.reduce_sum(y*tf.log(activation)) #cross entropy
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+# Construct model
+activation = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax
 
-# Train
+# Minimize error using cross entropy
+cost = -tf.reduce_sum(y*tf.log(activation)) # Cross entropy
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost) # Gradient Descent
+
+# Initializing the variables
 init = tf.initialize_all_variables()
+
+# Launch the graph
 with tf.Session() as sess:
     sess.run(init)
+
+    # Training cycle
     for epoch in range(training_epochs):
         avg_cost = 0.
         total_batch = int(mnist.train.num_examples/batch_size)
+        # Loop over all batches
         for i in range(total_batch):
             batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            # Fit training using batch data
             sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys})
+            # Compute average loss
             avg_cost += sess.run(cost, feed_dict={x: batch_xs, y: batch_ys})/total_batch
+        # Display logs per epoch step
         if epoch % display_step == 0:
             print "Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost)
 
     print "Optimization Finished!"
 
-    # Test trained model
-    correct_prediction = tf.equal(tf.argmax(activation,1), tf.argmax(y,1))
+    # Test model
+    correct_prediction = tf.equal(tf.argmax(activation, 1), tf.argmax(y, 1))
+    # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
     print "Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels})
