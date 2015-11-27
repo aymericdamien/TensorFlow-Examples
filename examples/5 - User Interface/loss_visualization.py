0d98a4476b4b0454ec4242629f6e3b50594a3e4f
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2015-11-27 16:24:39 +0900

    fix logdir name

--- examples/5 - User Interface/loss_visualization.py
+++ examples/5 - User Interface/loss_visualization.py
@@ -81,6 +81,6 @@ with tf.Session() as sess:
     print "Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels})
 
 '''
-Run the command line: tensorboard --logdir=/tmp/tensorflow
+Run the command line: tensorboard --logdir=/tmp/tensorflow_logs
 Open http://localhost:6006/ into your web browser
 '''
