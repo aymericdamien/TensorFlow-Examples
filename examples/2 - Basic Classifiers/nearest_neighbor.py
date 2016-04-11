9125684704ea5dd7d99b8de60a99d765e0d5abac
wenhu chen <hustchenwenhu@gmail.com>
2016-04-12 00:28:07 +0200

    Update nearest_neighbor.py
    Change all the computation to perform on device

--- examples/2 - Basic Classifiers/nearest_neighbor.py
+++ examples/2 - Basic Classifiers/nearest_neighbor.py
@@ -5,50 +5,43 @@ This example is using the MNIST database of handwritten digits (http://yann.lecu
 Author: Aymeric Damien
 Project: https://github.com/aymericdamien/TensorFlow-Examples/
 '''
-
 import numpy as np
 import tensorflow as tf
 
-# Import MINST data
 import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
-# In this example, we limit mnist data
-Xtr, Ytr = mnist.train.next_batch(5000) #5000 for training (nn candidates)
+mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
+
+Xtr, Ytr = mnist.train.next_batch(500)
 Xte, Yte = mnist.test.next_batch(200) #200 for testing
 
 # Reshape images to 1D
 Xtr = np.reshape(Xtr, newshape=(-1, 28*28))
+Ytr = np.reshape(Ytr, newshape=(-1,))
 Xte = np.reshape(Xte, newshape=(-1, 28*28))
+Yte = np.reshape(Yte, newshape=(-1,))
 
-# tf Graph Input
-xtr = tf.placeholder("float", [None, 784])
-xte = tf.placeholder("float", [784])
+xtr = tf.placeholder(tf.float32, [None, 784], name='xtr')
+ytr = tf.placeholder(tf.int32, [None,1], name='ytr')
+xte = tf.placeholder(tf.float32, [784], name='xte')
+yte = tf.placeholder(tf.int32, name='yte')
 
-# Nearest Neighbor calculation using L1 Distance
-# Calculate L1 Distance
-distance = tf.reduce_sum(tf.abs(tf.add(xtr, tf.neg(xte))), reduction_indices=1)
-# Predict: Get min distance index (Nearest neighbor)
-pred = tf.arg_min(distance, 0)
+distance = tf.reduce_sum(tf.abs(tf.sub(xtr, xte)), reduction_indices=1)
+value, indices = tf.nn.top_k(distance, 60)
+result = tf.nn.embedding_lookup(ytr, indices)
+y, _, count = tf.unique_with_counts(tf.reshape(result, (-1,)))
+idx = tf.argmax(count, 0)
+pred = tf.equal(tf.nn.embedding_lookup(y,idx), yte)
 
 accuracy = 0.
-
-# Initializing the variables
 init = tf.initialize_all_variables()
 
-# Launch the graph
 with tf.Session() as sess:
     sess.run(init)
-
-    # loop over test data
-    for i in range(len(Xte)):
-        # Get nearest neighbor
-        nn_index = sess.run(pred, feed_dict={xtr: Xtr, xte: Xte[i,:]})
-        # Get nearest neighbor class label and compare it to its true label
-        print "Test", i, "Prediction:", np.argmax(Ytr[nn_index]), "True Class:", np.argmax(Yte[i])
-        # Calculate accuracy
-        if np.argmax(Ytr[nn_index]) == np.argmax(Yte[i]):
+    for item, result in zip(Xte,Yte):
+        numpy_pred = sess.run(pred, feed_dict={xtr:Xtr, ytr:Ytr.reshape((-1,1)), xte:item, yte:result})
+        if numpy_pred:
             accuracy += 1./len(Xte)
     print "Done!"
-    print "Accuracy:", accuracy
+    print "Accuracy", accuracy
 
