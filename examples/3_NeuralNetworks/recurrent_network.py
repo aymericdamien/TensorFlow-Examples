8263daf4a28a49801a7ba6e9d37116490f07d918
Simanta Gautam <simantagautam@gmail.com>
2016-06-23 02:14:28 -0400

    fixed shape errors in the rnn model comments

--- examples/3_NeuralNetworks/recurrent_network.py
+++ examples/3_NeuralNetworks/recurrent_network.py
@@ -50,13 +50,13 @@ def RNN(x, weights, biases):
 
     # Prepare data shape to match `rnn` function requirements
     # Current data input shape: (batch_size, n_steps, n_input)
-    # Required shape: 'n_steps' tensors list of shape (batch_size, n_hidden)
+    # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
 
     # Permuting batch_size and n_steps
     x = tf.transpose(x, [1, 0, 2])
     # Reshaping to (n_steps*batch_size, n_input)
     x = tf.reshape(x, [-1, n_input])
-    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_hidden)
+    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
     x = tf.split(0, n_steps, x)
 
     # Define a lstm cell with tensorflow
