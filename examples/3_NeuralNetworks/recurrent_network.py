c6d8d68589e333949a1c823bfe0f2978a3f27607
Yunfeng Wang <wangyf11@mail.ustc.edu.cn>
2016-09-01 16:35:07 +0800

    fix typo: reccurent -> recurrent

--- examples/3_NeuralNetworks/recurrent_network.py
+++ examples/3_NeuralNetworks/recurrent_network.py
@@ -1,5 +1,5 @@
 '''
-A Reccurent Neural Network (LSTM) implementation example using TensorFlow library.
+A Recurrent Neural Network (LSTM) implementation example using TensorFlow library.
 This example is using the MNIST database of handwritten digits (http://yann.lecun.com/exdb/mnist/)
 Long Short Term Memory paper: http://deeplearning.cs.cmu.edu/pdfs/Hochreiter97_lstm.pdf
 
@@ -18,7 +18,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 '''
-To classify images using a reccurent neural network, we consider every image
+To classify images using a recurrent neural network, we consider every image
 row as a sequence of pixels. Because MNIST image shape is 28*28px, we will then
 handle 28 sequences of 28 steps for every sample.
 '''
