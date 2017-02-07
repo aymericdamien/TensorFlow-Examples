c0d51bf95313eb09721ddfd470b8876cb6d2b178
LazarValkov <lazarvalkov1@gmail.com>
2017-02-07 16:33:17 +0000

    updated the RNN function
    changed to dynamic_rnn and changed the way to get the last output of the rnn.

--- examples/3_NeuralNetworks/recurrent_network.py
+++ examples/3_NeuralNetworks/recurrent_network.py
@@ -46,28 +46,15 @@ biases = {
     'out': tf.Variable(tf.random_normal([n_classes]))
 }
 
-
 def RNN(x, weights, biases):
-
-    # Prepare data shape to match `rnn` function requirements
-    # Current data input shape: (batch_size, n_steps, n_input)
-    # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
-
-    # Permuting batch_size and n_steps
-    x = tf.transpose(x, [1, 0, 2])
-    # Reshaping to (n_steps*batch_size, n_input)
-    x = tf.reshape(x, [-1, n_input])
-    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
-    x = tf.split(x, n_steps, 0)
-
     # Define a lstm cell with tensorflow
-    lstm_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-
-    # Get lstm cell output
-    outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
-
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden)
+    outputs, states = tf.nn.dynamic_rnn(lstm_cell, x, dtype=tf.float32)
+    # [outputs' size is (?, 28, 128), we want the last element, with index 27
+    # size of last_output is then (?, 128)
+    last_output = outputs[:, outputs.get_shape()[1] - 1, :]
     # Linear activation, using rnn inner loop last output
-    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+    return tf.matmul(last_output, weights['out']) + biases['out']
 
 pred = RNN(x, weights, biases)
 
