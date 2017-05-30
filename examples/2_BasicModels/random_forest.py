09a17aa9c19e680d6c5ca204b8397c58698aac64
malleus17 <malleus17@gmail.com>
2017-05-30 01:37:15 +0100

    add kmeans + random forest

--- examples/2_BasicModels/random_forest.py
+++ examples/2_BasicModels/random_forest.py
@@ -19,25 +19,27 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=False)
 
 # Parameters
-num_steps = 10000 # Total steps to train
-batch_size = 512 # The number of samples per batch
+num_steps = 500 # Total steps to train
+batch_size = 1024 # The number of samples per batch
 num_classes = 10 # The 10 digits
 num_features = 784 # Each image is 28x28 pixels
-num_trees = 100
+num_trees = 10
 max_nodes = 1000
 
 # Input and Target data
 X = tf.placeholder(tf.float32, shape=[None, num_features])
-Y = tf.placeholder(tf.float32, shape=[None, num_classes])
+# For random forest, labels must be integers (the class id)
+Y = tf.placeholder(tf.int32, shape=[None])
 
 # Random Forest Parameters
 hparams = tensor_forest.ForestHParams(num_classes=num_classes,
                                       num_features=num_features,
                                       num_trees=num_trees,
                                       max_nodes=max_nodes).fill()
+
 # Build the Random Forest
 forest_graph = tensor_forest.RandomForestGraphs(hparams)
 # Get training graph and loss
@@ -46,24 +48,26 @@ loss_op = forest_graph.training_loss(X, Y)
 
 # Measure the accuracy
 infer_op = forest_graph.inference_graph(X)
-correct_prediction = tf.equal(tf.argmax(infer_op, 1), tf.argmax(Y, 1))
+correct_prediction = tf.equal(tf.argmax(infer_op, 1), tf.cast(Y, tf.int64))
 accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Initialize all variables
-init = tf.global_variables_initializer()
+init_vars = tf.global_variables_initializer()
 
-with tf.Session() as sess:
-    sess.run(init)
+# Start TensorFlow session
+sess = tf.Session()
+sess.run(init_vars)
 
-    # Training
-    for i in range(1, num_steps + 1):
-        # Prepare Data
-        # Get the next batch of MNIST data (only images are needed, not labels)
-        batch_x, batch_y = mnist.train.next_batch(batch_size)
-        _, l = sess.run([train_op, loss_op], feed_dict={X: batch_x, Y: batch_y})
-        if i % 100 == 0 or i == 1:
-            acc = sess.run(accuracy_op, feed_dict={X: batch_x, Y: batch_y})
-            print('Step %i, Loss: %f, Acc: %f' % (i, l, acc))
+# Training
+for i in range(1, num_steps + 1):
+    # Prepare Data
+    # Get the next batch of MNIST data (only images are needed, not labels)
+    batch_x, batch_y = mnist.train.next_batch(batch_size)
+    _, l = sess.run([train_op, loss_op], feed_dict={X: batch_x, Y: batch_y})
+    if i % 50 == 0 or i == 1:
+        acc = sess.run(accuracy_op, feed_dict={X: batch_x, Y: batch_y})
+        print('Step %i, Loss: %f, Acc: %f' % (i, l, acc))
 
-
-
+# Test Model
+test_x, test_y = mnist.test.images, mnist.test.labels
+print("Test Accuracy:", sess.run(accuracy_op, feed_dict={X: test_x, Y: test_y}))
